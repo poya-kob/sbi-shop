db4516da8f920da705d9f7dbe12bd4e8af471ac7
poya <poya_kob@live.com>
2023-01-16 12:14:54 +0330

    gallery app model created

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from utils import upload_image_path
+
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to=upload_image_path)
+    crated = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
