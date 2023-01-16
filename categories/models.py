88544753534b6e2d9cdf7fbdabd314a58338b5d2
poya <poya_kob@live.com>
2023-01-16 19:03:48 +0330

    category model tranclated

--- categories/models.py
+++ categories/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from django_jalali.db import models as jmodels
 from mptt import models as mp
 
 
 class Categories(mp.MPTTModel):
-    title = models.CharField(max_length=150)
-    parent = mp.TreeForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True)
+    title = models.CharField(max_length=150,verbose_name=_('title'))
+    parent = mp.TreeForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True,verbose_name=_('parent'))
     created_time = jmodels.jDateTimeField(auto_now_add=True)
 
     def __str__(self):
