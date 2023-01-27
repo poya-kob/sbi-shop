c090e447aae27eeef5e4e609f20da11c6b226423
poya <poya_kob@live.com>
2023-01-27 15:35:51 +0330

    static root and media root changed for server

--- sbi_shop/settings.py
+++ sbi_shop/settings.py
@@ -153,17 +153,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 if DEBUG:
     STATICFILES_DIRS = [(
             BASE_DIR / 'static/')
     ]
-else:
-    STATIC_ROOT = BASE_DIR / "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+    MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
+else:
+    STATIC_ROOT = "/home/public_html/smartsbi/static"
+    # STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+    MEDIA_ROOT = "/home/public_html/smartsbi/media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
